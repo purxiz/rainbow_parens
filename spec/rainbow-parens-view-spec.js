506468e81b0578dab01bdaee8edb161f60105be2
purxiz <functionofn@gmail.com>
2017-01-30 11:37:41 -0800

    test

---
+++ spec/rainbow-parens-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RainbowParensView from '../lib/rainbow-parens-view';
+
+describe('RainbowParensView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
